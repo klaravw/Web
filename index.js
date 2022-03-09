e01e97c109df6f341f0ccfb46755949261ea7945
klaravw <49321480+klaravw@users.noreply.github.com>
2022-03-08 21:14:13 -0500

    still no work :(

--- index.js
+++ index.js
@@ -1,8 +1,11 @@
-const wat = document.getElementsByClassName("whatever")[0];
+let wat = document.getElementsByClassName("whatever")[0];
 let toggle = false;
 console.log(wat);
-wat.onClick = function(){
-  var wat = document.getElementsByClassName("whatever")[0];
+wat.onClick = alert("f");
+//function(){wat.innerHTML = "wee"};
+function fun(){
+  //wat.innerHTML = "wee";
+  //var wat = document.getElementsByClassName("whatever")[0];
   toggle = !toggle;
   if(toggle){
     wat.innerHTML = "wee";
